df6065adc2b05d7c5581028d98bfe27ddca401c5
Hacke Wayne <hacklew@hotmail.com>
2021-01-02 17:58:47 +1300

    no bang

--- src/stateProvider.tsx
+++ src/stateProvider.tsx
@@ -31,20 +31,22 @@ export function StateProvider({ initialState, appIOFactory, children }: StatePro
 
 /**
  * @param Comp it's generally good practice to make this a pure component so it's possible to use React.memo(Comp)
- * @param mapToStateProps 
+ * @param mapStateToProps 
  */
 export function withState<POwn = {}, PSt = {}, P extends (POwn & PSt) = POwn & PSt>(
     Comp: React.FunctionComponent<P>, 
-    mapToStateProps: (state: State, appIO: AppIO, props?: POwn) => PSt
+    mapStateToProps: (state: State, appIO: AppIO, props?: POwn) => PSt
 ) {
-    // make sure this is set only once 
-    // to avoid creating a new object with appIOFactory(setState) every time 
     let appIO: AppIO | null = null;
     
     return (props: POwn) => {
         const { appIOFactory, state, setState } = useContext<StateContext>(store);
+
+        // this is set only once 
+        // to avoid creating a new object with appIOFactory(setState) every time 
         appIO = appIO ?? appIOFactory(setState);
-        const stateProps = mapToStateProps(state!, appIO, props);
+
+        const stateProps = mapStateToProps(state, appIO, props);
         const allProps: P = { ...props, ...stateProps } as P;
 
         return <Comp { ...allProps } />;
